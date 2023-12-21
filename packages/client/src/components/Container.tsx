f1b653bfded56cc718fde171ed63c81a8378f056
Hassan Ezz <hassan@heurolabs.com>
2023-12-21 13:42:14 +0200

    update: deleted the client

--- packages/client/src/components/Container.tsx
+++
@@ -1,5 +0,0 @@
-export default function Container({ children, className }: { children: React.ReactNode, className?: string }) {
-  return (
-    <div className={`container max-w-[1024px] m-auto px-5 ${className}`}>{children}</div>
-  )
-}
--- packages/client/src/pages/Profile.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "../components/Container"
-
-export default function Profile() {
-  return (
-    <Container>
-      Hello world
-    </Container>
-  )
-}
